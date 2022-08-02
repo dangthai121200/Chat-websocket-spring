c7b303c9a5590d4db9d7ce8a59553ad060bf1641
INI\thai.dang <thai.dang@terralogic.com>
2022-08-02 10:40:00 +0700

    first commit

---
+++ src/main/java/com/demo/ChatWebsocketSpringApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebsocketSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWebsocketSpringApplication.class, args);
+	}
+
+}
